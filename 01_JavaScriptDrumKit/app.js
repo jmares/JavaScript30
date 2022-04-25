08fab0e4c23c75bceb87b52d149407a9fda03cb2
jmares <67288966+jmares@users.noreply.github.com>
2022-04-25 20:03:17 +0200

    Javascript Drum Kit reorganized and documented code

--- 01_JavaScriptDrumKit/app.js
+++ 01_JavaScriptDrumKit/app.js
@@ -1,22 +1,38 @@
-function playSound(e) {
+/**
+ * Declarations
+ */
+ const keys = document.querySelectorAll('.key');
+ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+ 
+
+/**
+ * Remove the transition (class playing) after the transform, last transition, has finished
+ */
+ function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip it if it is not a transform
+    // e : the event
+    // this : the object on which the event occured
+    this.classList.remove('playing');
+}
+
+
+/**
+ * Play the sound corresponding to the key pressed
+ */
+ function playSound(e) {
     // console.log(e.keyCode + " " + e.code + " " + e.altKey + " " + e.charCode + " " + e.key);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return // stop function from running
-    audio.currentTime = 0; // rewind to the start;
+    audio.currentTime = 0; // rewind to the start, so you don't have to wait for the sound to finish;
     audio.play();
     key.classList.add('playing');
 }
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; // skip it if it is not a transform
-    // console.log(e);
-    // console.log(this);
-    this.classList.remove('playing');
-}
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+/**
+ * Main code
+ */
 
 window.addEventListener('keydown', playSound);
 
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Start: 25/04/2022
 ## 01. JavaScript Drum Kit
 
 - 25/04/2022: tutorial, code along, put javascript code in separate file
+- 25/04/2022: reorganized and documented code
